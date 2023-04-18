20881688c17287fd3c9bc583f1842377f4522d38
DylanOrange <you@example.com>
2023-04-18 23:15:59 +0200

    6dof-depth

--- droid_slam/geom/chol.py
+++ droid_slam/geom/chol.py
@@ -58,55 +58,20 @@ def block_solve(H, b, ep=0.0001, lm=0.0001):
     return x
 
 
-# def schur_solve(H, E, C, v, w, ep=0.0001, lm=0.0001, sless=False):
-#     """ solve using shur complement """
-    
-#     B, P, M, D, HW = E.shape #1,5,7,6,30*101
-#     H = H.permute(0,1,3,2,4).reshape(B, P*D, P*D)
-#     E = E.permute(0,1,3,2,4).reshape(B, P*D, M*HW)#1,5,6,7,30*101->1,30,7*30*101
-#     Q = (1.0 / C).view(B, M*HW, 1)
-
-#     # damping
-#     I = torch.eye(P*D).to(H.device)
-#     H = H + (ep + lm*H) * I
-    
-#     v = v.reshape(B, P*D, 1)
-#     w = w.reshape(B, M*HW, 1)
-
-#     Et = E.transpose(1,2)
-#     S = H - torch.matmul(E, Q*Et)
-#     v = v - torch.matmul(E, Q*w)
-
-#     # dx = CholeskySolver.apply(S, v)
-#     try:
-#         dx = torch.linalg.solve(S, v)
-#     except Exception as e:
-#         print(e)
-#         dx = torch.zeros_like(v)
-
-#     if sless:
-#         return dx.reshape(B, P, D)
-
-#     dz = Q * (w - Et @ dx)    
-#     dx = dx.reshape(B, P, D)
-#     dz = dz.reshape(B, M, HW)
-
-#     return dx, dz
-
 def schur_solve(H, E, C, v, w, ep=0.0001, lm=0.0001, sless=False):
     """ solve using shur complement """
     
-    B, K, M, HW = E.shape #1,5,7,6,30*101
-    # H = H.permute(0,1,3,2,4).reshape(B, P*D, P*D)
-    E = E.reshape(B, K, M*HW)#1,5,6,7,30*101->1,30,7*30*101
-    Q = 1.0 / C
+    B, P, M, D, HW = E.shape #1,5,7,6,30*101
+    H = H.permute(0,1,3,2,4).reshape(B, P*D, P*D)
+    E = E.permute(0,1,3,2,4).reshape(B, P*D, M*HW)#1,5,6,7,30*101->1,30,7*30*101
+    Q = (1.0 / C).view(B, M*HW, 1)
 
     # damping
-    I = torch.eye(K).to(H.device)
+    I = torch.eye(P*D).to(H.device)
     H = H + (ep + lm*H) * I
     
-    # v = v.reshape(B, P*D, 1)
-    # w = w.reshape(B, M*HW, 1)
+    v = v.reshape(B, P*D, 1)
+    w = w.reshape(B, M*HW, 1)
 
     Et = E.transpose(1,2)
     S = H - torch.matmul(E, Q*Et)
@@ -120,10 +85,45 @@ def schur_solve(H, E, C, v, w, ep=0.0001, lm=0.0001, sless=False):
         dx = torch.zeros_like(v)
 
     if sless:
-        return dx
+        return dx.reshape(B, P, D)
 
     dz = Q * (w - Et @ dx)    
-    dx = dx
+    dx = dx.reshape(B, P, D)
     dz = dz.reshape(B, M, HW)
 
     return dx, dz
+
+# def schur_solve(H, E, C, v, w, ep=0.0001, lm=0.0001, sless=False):
+#     """ solve using shur complement """
+    
+#     B, K, M, HW = E.shape #1,5,7,6,30*101
+#     # H = H.permute(0,1,3,2,4).reshape(B, P*D, P*D)
+#     E = E.reshape(B, K, M*HW)#1,5,6,7,30*101->1,30,7*30*101
+#     Q = 1.0 / C
+
+#     # damping
+#     I = torch.eye(K).to(H.device)
+#     H = H + (ep + lm*H) * I
+    
+#     # v = v.reshape(B, P*D, 1)
+#     # w = w.reshape(B, M*HW, 1)
+
+#     Et = E.transpose(1,2)
+#     S = H - torch.matmul(E, Q*Et)
+#     v = v - torch.matmul(E, Q*w)
+
+#     # dx = CholeskySolver.apply(S, v)
+#     try:
+#         dx = torch.linalg.solve(S, v)
+#     except Exception as e:
+#         print(e)
+#         dx = torch.zeros_like(v)
+
+#     if sless:
+#         return dx
+
+#     dz = Q * (w - Et @ dx)    
+#     dx = dx
+#     dz = dz.reshape(B, M, HW)
+
+#     return dx, dz
